7baceb04a7671a06c922ba4c19c615ad5fe65804
Simon Da Vall <simon.davall@gmail.com>
2024-07-19 20:17:30 +0100

    Nvim: Added comment to fix Comment bug

--- .config/nvim/lua/user/plugins/comment.lua
+++ .config/nvim/lua/user/plugins/comment.lua
@@ -1,4 +1,5 @@
---TODO Some of the keymaps seem borked, also check other languages like C# and C
+-- TODO: Some of the keymaps seem borked, also check other languages like C# and C
+-- see comment on the repository setup page
 return {
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -9,13 +10,15 @@ return {
     -- import comment plugin safely
     local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+    --FIX: this line was causing a 'too long' error
+    --local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
+    comment.setup()
     -- enable comment
-    comment.setup({
+    --comment.setup({
       -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
+      --pre_hook = ts_context_commentstring.create_pre_hook(),
+    --})
 
   end,
 }

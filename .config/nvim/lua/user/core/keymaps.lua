d57188a3d425f2568f548030c74c381e1ff450e9
Simon Da Vall <simon.davall@gmail.com>
2024-07-19 17:06:50 +0100

    Nvim: Fixed incorrect leader key usage in keymaps.lua

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -10,7 +10,7 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
-keymap.set("i", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })

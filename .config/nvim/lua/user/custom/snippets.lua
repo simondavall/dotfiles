4bb61e19ac8cf671111f5e6ad9dde2a6202e0a62
Simon Da Vall <simon.davall@gmail.com>
2024-07-28 13:16:45 +0100

    Changed custom snippets format

--- .config/nvim/lua/user/custom/snippets.lua
+++ .config/nvim/lua/user/custom/snippets.lua
@@ -1,22 +1,29 @@
+-- how to add custom snippets https://www.youtube.com/watch?v=FmHhonPjvvA
+
 local luasnip = require("luasnip")
 local s = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 M = {}
 
 function M.load()
 
   luasnip.add_snippets("c", {
-    s("main_sdv", {
-      t("#include <stdio.h>"),
-      t({"", ""}),
-      t({"", "int main(void){"}),
-      t({"", "\t"}), i(0),
-      t({"", "\tprintf(\"Answer:\\n\");"}),
-      t({"", "\treturn 0;"}),
-      t({"", "}"}),
-    })
+    s("main_sdv", fmt(
+      [[
+      #include <stdio.h>
+
+      int main(void){{
+        {}
+
+        printf("Answer\n");
+        return 0;
+      }}
+      ]], {
+        i(0)
+      }))
   })
 
 end

ed9deab4e4a5ed4d67432b31d47ce0fcf4ecac83
Simon Da Vall <simon.davall@gmail.com>
2024-10-08 09:28:59 +0100

    Removed tsserver from lsp list

--- .config/nvim/lua/user/plugins/lsp/mason.lua
+++ .config/nvim/lua/user/plugins/lsp/mason.lua
@@ -28,7 +28,7 @@ return {
       -- list of servers for mason to install
       ensure_installed = {
         "clangd",
-        "tsserver",
+        -- "tsserver",
         "html",
         "cssls",
         "tailwindcss",

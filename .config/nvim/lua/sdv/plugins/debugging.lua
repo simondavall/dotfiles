ba0ce16a5bb6d4c2bc3da6a3bc12b49fcb4ed65c
Simon Da Vall <simon.davall@gmail.com>
2026-02-20 09:33:44 +0000

    nvim: tweaks

--- .config/nvim/lua/sdv/plugins/debugging.lua
+++ .config/nvim/lua/sdv/plugins/debugging.lua
@@ -41,16 +41,16 @@ return {
           -- --   -- todo: request input from ui
           -- --   return "/path/to/your.dll"
           -- -- end,
-          -- env = {
-          --   ASPNETCORE_ENVIRONMENT = function()
-          --     -- todo: request input from ui
-          --     return "Development"
-          --   end,
-          --   ASPNETCORE_URLS = function()
-          --     -- todo: request input from ui
-          --     return "http://localhost:5050"
-          --   end,
-          -- },
+          env = {
+            ASPNETCORE_ENVIRONMENT = function()
+              -- todo: request input from ui
+              return "Development"
+            end,
+            ASPNETCORE_URLS = function()
+              -- todo: request input from ui
+              return "https://localhost:7069"
+            end,
+          },
           -- cwd = function()
           --   -- todo: request input from ui
           --   return vim.fn.getcwd()
--- .config/nvim/lua/sdv/plugins/lsp/lspsignature.lua
+++ .config/nvim/lua/sdv/plugins/lsp/lspsignature.lua
@@ -4,8 +4,9 @@ return {
   opts = {
     bind = true,
     -- floating_window = true,
-    -- floating_window_above_cur_line = true,
-    floating_window_off_y = -2, -- move the floating window up 2 lines to prevent hiding cursor
+    floating_window_above_cur_line = true, -- open window below cursor
+    floating_window_off_y = -5, -- move the floating window down 5 lines to prevent hiding cursor
+    floating_window_off_x = 20,
     hint_enable = false, -- optional: reduces visual noise
     handler_opts = {
       border = "rounded"
